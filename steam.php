90ffe68eeccd1a70ab171aa96ae2d44cb7a1189e
Thomas Rixon <Thomas Rixon>
2016-03-05 16:17:28 +0000

    Code.

--- steam.php
+++ steam.php
@@ -8,11 +8,11 @@ $client = new Client();
 
 
 $crawler = $client->request('GET', 'https://steamcommunity.com/market');
-$href = $crawler->filterXpath('//div[@id="popularItemsRows"]/a')->extract(array('href'));
-
+//$href = $crawler->filterXpath('//div[@id="popularItemsRows"]/a')->extract(array('href'));
+$href = $crawler->selectLink('Counter-Strike: Global Offensive')->link();
 foreach ($href as $link) {
-  $gohere = $link->link();
-  $crawler = $client->click($gohere);
+  //$gohere = $link->link();
+  $crawler = $client->click($link);
   //print '<li>' . $link . '<li>';
   $name = $crawler->filterXpath('//h1[@id="largeiteminfo_item_name"]')->text();
   print '<li>' . $name . '<li>';
