61c1c7b851e3e1a08644adc2c479716be0981092
Thomas Rixon <Thomas Rixon>
2016-03-05 16:14:14 +0000

    Code.

--- steam.php
+++ steam.php
@@ -11,8 +11,11 @@ $crawler = $client->request('GET', 'https://steamcommunity.com/market');
 $href = $crawler->filterXpath('//div[@id="popularItemsRows"]/a')->extract(array('href'));
 
 foreach ($href as $link) {
-  //$crawler = $client->click($link);
-  print '<li>' . $link . '<li>';
+  $gohere = $link->link();
+  $crawler = $client->click($gohere);
+  //print '<li>' . $link . '<li>';
+  $name = $crawler->filterXpath('//h1[@id="largeiteminfo_item_name"]')->text();
+  print '<li>' . $name . '<li>';
 }
 
 print 'This is a test';
